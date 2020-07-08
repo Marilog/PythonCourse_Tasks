6c817d63f6103103bf5a5df3d67ea0ff76441d1c
Mariana Logvinenko <mariana.logvinenko@gmail.com>
2020-07-08 14:44:58 +0200

    HW_1 added

---
+++ HW_1.1.py
@@ -0,0 +1,15 @@
+#1.task
+
+var_1 = int(input("Define 1. variable:"))
+var_2 = int(input("Define 2.variable:"))
+
+sum = var_1+var_2
+subtr = var_1-var_2
+mult = var_1*var_2
+div = var_1/var_2
+
+print("The result of sum of two numbers is ", sum)
+print("The result of subtraction of two numbers is ", subtr)
+print("The result of multiplication of two numbers is ", mult)
+print("The result of division of two numbers is ", div)
+
---
+++ HW_1.2.py
@@ -0,0 +1,10 @@
+#2.task
+
+name = str(input("What is your name?"))
+age = str(input("How old are you?"))
+residence = str(input("Where do you live?"))
+
+
+print("Hello, ", name)
+print("Your age is ", age)
+print("You live in ", residence)
