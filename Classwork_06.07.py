71dafd5492e845021b5cd4559cf8fed854291bcc
Mariana Logvinenko <mariana.logvinenko@gmail.com>
2020-07-11 23:24:40 +0200

    HW_3.2 added

--- Classwork_06.07.py
+++
@@ -1,2 +0,0 @@
-list_2=range(10)
-print("Odd numbers are ", list_2[0,10,2])
---
+++ HW_3.2.py
@@ -0,0 +1,6 @@
+def reverse(st):
+   
+    splitted_list = st.split()
+    reversed_list = " ".join(reversed(splitted_list))
+    
+    return reversed_list
