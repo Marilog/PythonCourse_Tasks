183bfb801c23e67d2e9fcdd790f06feb159fa3cf
Mariana Logvinenko <mariana.logvinenko@gmail.com>
2020-07-12 16:54:57 +0200

    HW_4.3 added

---
+++ HW_4.2.py
@@ -0,0 +1,9 @@
+def enough(cap, on, wait):
+    cap = 50
+    on = 0
+    wait = cap - on
+    while on < cap:
+      return 0
+      on += 1
+    else:
+        return wait
---
+++ HW_4.3.py
@@ -0,0 +1,8 @@
+def areYouPlayingBanjo(name):
+    
+    
+      if name.startswith('R') or name.startswith('r'):
+        return "{} plays banjo".format(name)
+    
+      else:
+          return "{} does not play banjo".format(name)
